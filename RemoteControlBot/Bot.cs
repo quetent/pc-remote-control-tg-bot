d4ccef4c06f7470a808252320c091cfc2e4e366c
awen <quetent@icloud.com>
2023-01-29 23:40:21 +0600

    create bot instance

---
+++ RemoteControlBot/Bot.cs
@@ -0,0 +1,63 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace RemoteControlBot
+{
+    internal class Bot
+    {
+        private readonly TelegramBotClient _botClient;
+        private readonly ReceiverOptions _receiverOptions;
+
+        private readonly CancellationToken _cancellationToken;
+
+        public Bot(string token, ReceiverOptions recieverOptions, CancellationToken cancellationToken)
+        {
+            _botClient = new TelegramBotClient(token);
+            _receiverOptions = recieverOptions;
+            _cancellationToken = cancellationToken;
+        }
+
+        public void Start()
+        {
+            _botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: _receiverOptions,
+                cancellationToken: _cancellationToken);
+        }
+
+        private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is not { } message)
+                return;
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        private static async Task<Task> HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var exceptionMessage = exception switch
+            {
+                ApiRequestException apiRequestException 
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(exceptionMessage);
+
+            return Task.CompletedTask;
+        }
+    }
+}
