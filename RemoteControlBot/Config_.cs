f4f80671f303a3cf945d1be1a0503932e68bc44a
awen <quetent@icloud.com>
2023-02-09 20:07:22 +0600

    config example add

---
+++ RemoteControlBot/Config_.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing.Imaging;
+
+namespace RemoteControlBot
+{
+    internal static class Config_
+    {
+        internal const long OWNER_ID = default;
+
+        internal const int FINALIZE_WAITING_TIME_MS = default;
+
+        internal const bool AUTO_RESTART_IF_CRASHED = default;
+        internal const bool AUTO_RESTART_IF_CONNECTION_LOST = default;
+        internal const bool ENABLE_LOGGING = default;
+        internal const bool LOG_TO_CONSOLE = default;
+        internal const bool LOG_TO_FILE = default;
+
+        internal const string INTERNET_CHECKING_URL = default;
+
+        internal const string BOT_NAME = default;
+        internal const string TOKEN = default;
+
+        internal const string SCREENSHOT_SAVE_DIR = default;
+        internal const string SCREENSHOT_FILENAME = default;
+        internal static readonly ImageFormat SCREENSHOT_FORMAT = default;
+
+        internal const string LOG_FILENAME = default;
+        internal const string BUG_REPORT_FILENAME = default;
+    }
+}
